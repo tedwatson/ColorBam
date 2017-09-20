d95668131f9cf9cd2f7761df4999740552ad5016
Ted Watson <tedwatson123@mac.com>
2017-09-20 16:33:10 -0400

    implemented gun firing functionality

---
+++ Assets/Scripts/gunController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunController : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject bulletSpawnPoint;
+
+	public void fireGun() 
+	{
+		// instantiate a new bullet at the spawn point
+		Instantiate(bullet, bulletSpawnPoint.transform.position, transform.rotation);
+	}
+}
