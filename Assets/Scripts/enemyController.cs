928a27e72ea27f29a8decfa03220a1fbff437fb8
shreyavi <shreyavi@usc.edu>
2017-09-26 05:32:19 -0500

    Added comments and more on toggeling UI
    Added comments and more on toggeling UI

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -12,39 +12,38 @@ public class enemyController : MonoBehaviour {
 
     private int index = 1;//Initialize at 1, otherwise you have to hit the object twice to change colors at first.
 
-	public GameObject target;
-	public Text countText;
-	private static int count; 
-	public AudioClip[] soundFiles;
-	public AudioSource soundSource;
-	private List<string> doneColors = new List<string>();
+	public GameObject target;//reference for target object
+	public Text countText;//refernece for Score Text
+	private static int count; //Keeping the score static so that changes are reflected everywhere 
+	public AudioClip[] soundFiles;//reference to audio files to be used
+	public AudioSource soundSource;//reference to Audio Source
+	private List<string> doneColors = new List<string>(); //A list to store the colors that have been already matched
     // Use this for initialization
     void Start()
     {
         Rend = GetComponent<Renderer>();//Gives functionality for the renderer
         Rend.enabled = true;//Makes the rendered 3d object visable if enabled;
-		count = 0;
-		SetCountText();
+		count = 0;//initializing the count to zero
+		SetCountText();//Setting the score in UI
     }
 
 
     void OnTriggerEnter(Collider c) {
-		
-		string bulletTag = c.GetComponent<Renderer> ().material.name.Replace ("(Instance)", "").ToString ().Trim();
+		string bulletTag = c.GetComponent<Renderer> ().material.name.Replace ("(Instance)", "").ToString ().Trim();//A bullet's tag would be the name of the material of the renderer
 		Debug.Log (bulletTag);
 		// if the bullet is the same color, we've scored!
 		if (bulletTag == target.gameObject.tag) {
 			//if one color is done then hitting the same target wont increase the score.
 			if (!doneColors.Contains(bulletTag))
 			{
-				doneColors.Add (bulletTag);
-				target.GetComponent<Renderer> ().material = c.GetComponent<Renderer> ().material;
+				doneColors.Add (bulletTag);//Matched colors are added to the list of colors that are done
+				target.GetComponent<Renderer> ().material = c.GetComponent<Renderer> ().material;//Giving the target the same color as the color matched.
 				//poof sound
 				//soundSource.PlayOneShot(soundFiles[0]);
 				Debug.Log ("collided");
-				count = count + 1;
+				count = count + 1;//Incrementing the count
 				Debug.Log (count);
-				SetCountText ();
+				SetCountText ();// Setting the score on the score board
 			}
 
 		} 
